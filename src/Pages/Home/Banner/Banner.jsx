9ce9df300126f217ebba5c537e3c3755880d1ff3
Ananda Das <65402344+Ananda1405@users.noreply.github.com>
2023-10-24 14:03:53 +0600

    Style Header & create simple banner

---
+++ src/Pages/Home/Banner/Banner.jsx
@@ -0,0 +1,57 @@
+import bg1 from "../../../assets/images/homeCarousel/1.jpg";
+import bg2 from "../../../assets/images/homeCarousel/2.jpg";
+import bg3 from "../../../assets/images/homeCarousel/3.jpg";
+import bg4 from "../../../assets/images/homeCarousel/4.jpg";
+
+const Banner = () => {
+  return (
+    <div className="carousel w-full">
+      <div id="slide1" className="carousel-item relative w-full">
+        <img src={bg1} className="w-full" />
+        <div className="absolute flex gap-3 justify-end transform -translate-y-1/2 left-5 right-5 bottom-0">
+          <a href="#slide4" className="btn btn-circle">
+            ❮
+          </a>
+          <a href="#slide2" className="btn btn-circle">
+            ❯
+          </a>
+        </div>
+      </div>
+      <div id="slide2" className="carousel-item relative w-full">
+        <img src={bg2} className="w-full" />
+        <div className="absolute flex gap-3 justify-end transform -translate-y-1/2 left-5 right-5 bottom-0">
+          <a href="#slide1" className="btn btn-circle">
+            ❮
+          </a>
+          <a href="#slide3" className="btn btn-circle">
+            ❯
+          </a>
+        </div>
+      </div>
+      <div id="slide3" className="carousel-item relative w-full">
+        <img src={bg3} className="w-full" />
+        <div className="absolute flex gap-3 justify-end transform -translate-y-1/2 left-5 right-5 bottom-0">
+          <a href="#slide2" className="btn btn-circle">
+            ❮
+          </a>
+          <a href="#slide4" className="btn btn-circle">
+            ❯
+          </a>
+        </div>
+      </div>
+      <div id="slide4" className="carousel-item relative w-full">
+        <img src={bg4} className="w-full" />
+        <div className="absolute flex gap-3 justify-end transform -translate-y-1/2 left-5 right-5 bottom-0">
+          <a href="#slide3" className="btn btn-circle">
+            ❮
+          </a>
+          <a href="#slide1" className="btn btn-circle">
+            ❯
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
