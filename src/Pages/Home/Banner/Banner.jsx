54de32a11a40170b67368cf8da94cb33376e702f
Ananda Das <65402344+Ananda1405@users.noreply.github.com>
2023-10-24 17:51:04 +0600

    Style Banner Component with gradient overlay

--- src/Pages/Home/Banner/Banner.jsx
+++ src/Pages/Home/Banner/Banner.jsx
@@ -1,3 +1,5 @@
+//https://www.youtube.com/watch?v=Ojb8OQCBqFs&ab_channel=TechEnthusiast
+
 import bg1 from "../../../assets/images/homeCarousel/1.jpg";
 import bg2 from "../../../assets/images/homeCarousel/2.jpg";
 import bg3 from "../../../assets/images/homeCarousel/3.jpg";
@@ -7,7 +9,17 @@ const Banner = () => {
   return (
     <div className="carousel w-full">
       <div id="slide1" className="carousel-item relative w-full">
-        <img src={bg1} className="w-full" />
+        <img src={bg1} className="w-full rounded-xl" />
+        <div className="absolute flex items-center bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0.00)] left-0 top-0 h-full">
+          <div className="w-1/2 text-white space-y-7 ml-28">
+            <h1 className="text-6xl font-bold">Affordable Price For Car Servicing</h1>
+            <p>There are many variations of passages of available, but the majority have suffered alteration in some form</p>
+            <div>
+              <button className="btn btn-primary mr-5">Discover More</button>
+              <button className="btn text-white btn-outline">Latest Project</button>
+            </div>
+          </div>
+        </div>
         <div className="absolute flex gap-3 justify-end transform -translate-y-1/2 left-5 right-5 bottom-0">
           <a href="#slide4" className="btn btn-circle">
             ❮
@@ -19,6 +31,16 @@ const Banner = () => {
       </div>
       <div id="slide2" className="carousel-item relative w-full">
         <img src={bg2} className="w-full" />
+        <div className="absolute flex items-center bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0.00)] left-0 top-0 h-full">
+          <div className="w-1/2 text-white space-y-7 ml-28">
+            <h1 className="text-6xl font-bold">Affordable Price For Car Servicing</h1>
+            <p>There are many variations of passages of available, but the majority have suffered alteration in some form</p>
+            <div>
+              <button className="btn btn-primary mr-5">Discover More</button>
+              <button className="btn text-white btn-outline">Latest Project</button>
+            </div>
+          </div>
+        </div>
         <div className="absolute flex gap-3 justify-end transform -translate-y-1/2 left-5 right-5 bottom-0">
           <a href="#slide1" className="btn btn-circle">
             ❮
@@ -30,6 +52,16 @@ const Banner = () => {
       </div>
       <div id="slide3" className="carousel-item relative w-full">
         <img src={bg3} className="w-full" />
+        <div className="absolute flex items-center bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0.00)] left-0 top-0 h-full">
+          <div className="w-1/2 text-white space-y-7 ml-28">
+            <h1 className="text-6xl font-bold">Affordable Price For Car Servicing</h1>
+            <p>There are many variations of passages of available, but the majority have suffered alteration in some form</p>
+            <div>
+              <button className="btn btn-primary mr-5">Discover More</button>
+              <button className="btn text-white btn-outline">Latest Project</button>
+            </div>
+          </div>
+        </div>
         <div className="absolute flex gap-3 justify-end transform -translate-y-1/2 left-5 right-5 bottom-0">
           <a href="#slide2" className="btn btn-circle">
             ❮
@@ -41,6 +73,16 @@ const Banner = () => {
       </div>
       <div id="slide4" className="carousel-item relative w-full">
         <img src={bg4} className="w-full" />
+        <div className="absolute flex items-center bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0.00)] left-0 top-0 h-full">
+          <div className="w-1/2 text-white space-y-7 ml-28">
+            <h1 className="text-6xl font-bold">Affordable Price For Car Servicing</h1>
+            <p>There are many variations of passages of available, but the majority have suffered alteration in some form</p>
+            <div>
+              <button className="btn btn-primary mr-5">Discover More</button>
+              <button className="btn text-white btn-outline">Latest Project</button>
+            </div>
+          </div>
+        </div>
         <div className="absolute flex gap-3 justify-end transform -translate-y-1/2 left-5 right-5 bottom-0">
           <a href="#slide3" className="btn btn-circle">
             ❮
