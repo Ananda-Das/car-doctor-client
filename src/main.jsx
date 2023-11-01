9d79e48799f83597bc0ac7552cc7858fb867bd53
Ananda Das <65402344+Ananda1405@users.noreply.github.com>
2023-11-01 20:24:01 +0600

    Bokking token added

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,8 @@ import { RouterProvider } from "react-router-dom";
 import router from "./Routes/Routes.jsx";
 import AuthProvider from "./Providers/AuthProvider";
 
+
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <div className="max-w-7xl mx-auto">
     <AuthProvider>
